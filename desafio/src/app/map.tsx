aa149432c190865bd86f7119123c78ba30058cd3
Deadpool345 <87096532+Deadpool345@users.noreply.github.com>
2026-09-11 19:49:18 -0600

    cambios minimos

--- desafio/src/app/map.tsx
+++ desafio/src/app/map.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { LocationMap } from "@/components/LocationMap";
 import { useAudit } from "@/context/AudioContext";
 
 export default function MapScreen() {
-    const {entries} = useAudit();
-    return <SafeAreaView style={styles.page}> <LocationMap entries={entries} /> </SafeAreaView>;
+    const { entries } = useAudit();
+    return (
+        <SafeAreaView style={styles.page}>
+            <LocationMap entries={entries} />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
     page: {
-        flex:1
+        flex: 1
     }
 });
